2ecd040d8f34ac129bce7875452717770495bf70
pedro <pedro@PEDROIGHORNOTEB>
2026-04-19 09:45:11 -0300

    Criação das classes LogExtensions,, CustomOperationDescriptions, ExceptionMiddleware, appsettings e program.

---
+++ FCG.Platform/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,75 @@
+﻿using FCG.Platform.Extensions.SwaggerDocumentation;
+using FCG.Platform.Infrastracture.Connections;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace FCG.Platform.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(opt =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                opt.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "API FCG.Platform",
+                    Description = @"
+                        A **API FCG.Platform** é uma solução moderna para gerenciamento de usuários e bibliotecas de jogos digitais. 
+                        Desenvolvida como base da plataforma **FIAP Cloud Games (FCG)**, ela permite o controle eficiente de contas, 
+                        autenticação segura e organização dos jogos adquiridos pelos usuários.
+
+                        **Principais Benefícios:**
+                        - Cadastro e gerenciamento de usuários com autenticação JWT.
+                        - Controle da biblioteca de jogos adquiridos.
+                        - Segurança com autenticação e autorização por níveis de acesso.
+                        - Estrutura preparada para evolução com novas funcionalidades como matchmaking e servidores online.
+
+                        Com a **FCG.Platform**, a base da plataforma de games educacionais é construída de forma escalável, 
+                        segura e pronta para crescimento!
+                        ",
+                });
+
+                opt.OperationFilter<CustomOperationDescriptions>();
+            });
+
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlServer(config.GetConnectionString("WebApiDatabase"));
+            });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:4200");
+                });
+            });
+
+            //services.AddScoped<IRepositoryUoW, RepositoryUoW>();
+            //services.AddScoped<TokenService>();
+            //services.AddScoped<BCryptoAlgorithm>();
+            //services.AddScoped<IUnitOfWorkService, UnitOfWorkService>();
+            //services.AddScoped<AuthService>();
+            //services.AddScoped<IUserService, UserService>();
+            //services.AddScoped<IUserRepository, UserRepository>();
+            //services.AddScoped<TokenService>();
+            //services.AddScoped<BCryptoAlgorithm>();
+
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            });
+
+            return services;
+        }
+    }
+}
---
+++ FCG.Platform/Extensions/SwaggerDocumentation/CustomOperationDescriptions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace FCG.Platform.Extensions.SwaggerDocumentation
+{
+    public class CustomOperationDescriptions : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (context?.ApiDescription?.HttpMethod is null || context.ApiDescription.RelativePath is null)
+                return;
+
+            var path = context.ApiDescription.RelativePath.ToLowerInvariant();
+
+            var routeHandlers = new Dictionary<string, Action>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "user",               () => HandleUserOperations(operation, context) }
+            };
+
+            foreach (var kv in routeHandlers
+                     .OrderByDescending(k => k.Key.Length))
+            {
+                if (path.Contains(kv.Key))
+                {
+                    kv.Value.Invoke();
+                    return;
+                }
+            }
+        }
+
+        private void HandleUserOperations(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var method = context.ApiDescription.HttpMethod;
+            var path = context.ApiDescription.RelativePath?.ToLower() ?? string.Empty;
+
+            if (method == "POST")
+            {
+                operation.Summary = "Create a new User.";
+                operation.Description = "This endpoint allows you to create a new User by providing the necessary details.";
+                AddResponses(operation, "200", "The User was successfully created.");
+            }
+            else if (method == "PUT")
+            {
+                operation.Summary = "Update an existing User.";
+                operation.Description = "This endpoint allows you to update an existing User by providing the necessary details.";
+                AddResponses(operation, "200", "The User was successfully updated.");
+            }
+            else if (method == "DELETE")
+            {
+                operation.Summary = "Delete an existing User.";
+                operation.Description = "This endpoint allows you to delete an existing User by providing the ID.";
+                AddResponses(operation, "200", "The User was successfully deleted.");
+                AddResponses(operation, "404", "User not found. Please verify the ID.");
+            }
+            else if (method == "GET")
+            {
+                operation.Summary = "Retrieve all Users.";
+                operation.Description = "This endpoint allows you to retrieve details of all existing Users.";
+                AddResponses(operation, "200", "All Users details were successfully retrieved.");
+            }
+        }
+
+        private void AddResponses(OpenApiOperation operation, string statusCode, string description)
+        {
+            if (!operation.Responses.ContainsKey(statusCode))
+            {
+                operation.Responses.Add(statusCode, new OpenApiResponse { Description = description });
+            }
+        }
+    }
+}
