47101b45167ba906841c98589755d1b1f4b23716
pedro <pedro@PEDROIGHORNOTEB>
2026-04-19 09:46:14 -0300

    Criação da classe UserController, classe program.

---
+++ FCG.Platform/Controllers/UserController.cs
@@ -0,0 +1,6 @@
+﻿namespace FCG.Platform.Controllers
+{
+    public class UserController
+    {
+    }
+}
--- FCG.Platform/Program.cs
+++ FCG.Platform/Program.cs
@@ -1,25 +1,74 @@
+using FCG.Platform.Extensions;
+using FCG.Platform.Extensions.ExtensionsLogs;
+using FCG.Platform.Infrastracture.Connections;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.CustomSchemaIds(t => t.FullName);
+    c.ResolveConflictingActions(d => d.First());
+});
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
+builder.Services.AddCors(o => o.AddDefaultPolicy(p =>
+    p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddAuthorization();
+
+LogExtension.InitializeLogger();
+var loggerSerialLog = LogExtension.GetLogger();
+loggerSerialLog.Information("Logging initialized.");
 
 var app = builder.Build();
+app.UseMiddleware<ExceptionMiddleware>();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
+    });
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+    app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors();
+app.UseSession();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<DataContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occured during migration!");
+    }
+}
 
 app.Run();
