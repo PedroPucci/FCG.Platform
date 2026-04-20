b2162e47bcbd19b5ffe110f0d03ad0624b8472a1
pedro <pedro@PEDROIGHORNOTEB>
2026-04-20 10:36:07 -0300

    Ajustando a classe de LogExtension para gerar arquivos diários e sem deletar o do dia anterior.

--- FCG.Platform/Extensions/ExtensionsLogs/LogExtension.cs
+++ FCG.Platform/Extensions/ExtensionsLogs/LogExtension.cs
@@ -26,21 +26,11 @@ namespace FCG.Platform.Extensions.ExtensionsLogs
                     .WriteTo.Console(LogEventLevel.Debug)
                     .WriteTo.File(Path.Combine(LogDirectory, "log-.txt"), rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 1000000)
                     .CreateLogger();
-
-                DeletePreviousLogFile();
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Failed to start the logger: {ex.Message}");
             }
-        }
-
-        private static void DeletePreviousLogFile()
-        {
-            var yesterdayDate = DateTime.Today.AddDays(-1).ToString("yyyyMMdd");
-            var logFilePath = Path.Combine(LogDirectory, $"log-{yesterdayDate}.txt");
-            if (File.Exists(logFilePath))
-                File.Delete(logFilePath);
         }
 
         public static ILogger GetLogger()
