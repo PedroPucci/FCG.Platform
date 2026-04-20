1090705e966e189acedfa03e791c28101f3589e8
pedro <pedro@PEDROIGHORNOTEB>
2026-04-19 21:44:18 -0300

    Criação do método Add para User.

--- FCG.Platform.Application/Services/UserService.cs
+++ FCG.Platform.Application/Services/UserService.cs
@@ -1,6 +1,10 @@
 ﻿using FCG.Platform.Domain.Entities.Entity;
 using FCG.Platform.Domain.Interfaces.Services;
+using FCG.Platform.Domain.OperationResult;
 using FCG.Platform.Infrastracture.Repository.RepositoryUoW;
+using FCG.Platform.Shared.Logging;
+using FCG.Platform.Shared.Validator;
+using Serilog;
 
 namespace FCG.Platform.Application.Services
 {
@@ -13,29 +17,63 @@ namespace FCG.Platform.Application.Services
             _repositoryUoW = repositoryUoW;
         }
 
-        public Task<UserEntity> Add(UserEntity userEntity)
+        public async Task<Result<UserEntity>> Add(UserEntity userEntity)
+        {
+            using var transaction = _repositoryUoW.BeginTransaction();
+
+            try
+            {
+                var isValid = await IsValidUserRequest(userEntity);
+                if (!isValid.Success)
+                    return Result<UserEntity>.Error(isValid.Message);
+
+                await _repositoryUoW.UserRepository.Add(userEntity);
+                await _repositoryUoW.SaveAsync();
+                await transaction.CommitAsync();
+
+                Log.Information(LogMessages.AddingUserSuccess());
+                return Result<UserEntity>.Ok();
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                Log.Error(LogMessages.AddingUserError(ex));
+                return Result<UserEntity>.Error($"Error to add a new User: {ex.Message}");
+            }
+        }
+
+        public async Task<Result<UserEntity>> Update(UserEntity userEntity)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> Delete(int id)
+        public async Task<bool> Delete(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<UserEntity>> Get()
+        public async Task<List<UserEntity>> Get()
         {
             throw new NotImplementedException();
         }
 
-        public Task<UserEntity> GetById(int id)
+        public async Task<Result<UserEntity>> GetById(int id)
         {
             throw new NotImplementedException();
-        }
+        }        
 
-        public Task<UserEntity> Update(UserEntity userEntity)
+        private async Task<Result<UserEntity>> IsValidUserRequest(UserEntity userEntity)
         {
-            throw new NotImplementedException();
+            var requestValidator = await new UserRequestValidator().ValidateAsync(userEntity);
+
+            if (!requestValidator.IsValid)
+            {
+                string errorMessage = string.Join(" ", requestValidator.Errors.Select(e => e.ErrorMessage));
+                errorMessage = errorMessage.Replace(Environment.NewLine, "");
+                return Result<UserEntity>.Error(errorMessage);
+            }
+
+            return Result<UserEntity>.Ok();
         }
     }
 }
